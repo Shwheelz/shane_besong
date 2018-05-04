65e28aaa51bc5cec39e2c8c545bd9486a3bcdffc
Shane Besong <shane@Shanes-iMac.local>
2018-05-03 20:30:17 -0500

    still tweaking dist

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// serves the static files in the dist directory
+// serve the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
-
-// start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+app.listen(process.env.PORT || 8080);
