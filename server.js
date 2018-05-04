535e6e7844e6216a89effc0f75f5c915b784d924
Shane Besong <shane@Shanes-iMac.local>
2018-05-03 23:40:19 -0500

    i think it finally works :3

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const path = require('path');
 
 // serve the static files in the dist directory
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static('./dist/shanebesong'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join('./dist/shanebesong/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
