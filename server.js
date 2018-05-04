ba169ff90a30f4e6669edc803d12c26a35b8b7a1
Shane Besong <shane@Shanes-iMac.local>
2018-05-03 20:26:21 -0500

    handle pathlocationstrategy routing

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // serves the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
 // start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
