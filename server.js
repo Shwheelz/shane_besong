55f968be3ecf2f56e007c0e36f0084c111e0488d
Shane Besong <shane@Shanes-iMac.local>
2018-05-03 20:43:45 -0500

    oops forgot to add path to package

--- server.js
+++ server.js
@@ -5,8 +5,4 @@ const path = require('path');
 // serve the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
 app.listen(process.env.PORT || 8080);
